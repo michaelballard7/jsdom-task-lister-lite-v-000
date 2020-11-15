ca9772433c9e6d30d2d059ca2b235bda56701bb0
Michael Ballard <ballard.michael9@gmail.com>
2020-11-15 22:22:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-
+    
   let userText = document.querySelector('#new-task-description');
 
   let form = document.querySelector('#create-task-form');
