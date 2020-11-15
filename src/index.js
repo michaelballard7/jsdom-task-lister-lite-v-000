f4e209a4bd75da79fa540cc9586a9f20b088f815
Michael Ballard <ballard.michael9@gmail.com>
2020-11-15 22:32:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -29,7 +29,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   taskContainer.addEventListener('click', (e) => {
-      console.log(e.target)
+      console.log(e.target.classList)
+
+
   })
 
 
