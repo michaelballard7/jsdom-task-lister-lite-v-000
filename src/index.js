2b3d0902e63816ea90fa6ad66722f563aed86f18
Michael Ballard <ballard.michael9@gmail.com>
2020-11-15 22:21:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,11 +9,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   form.addEventListener('submit', (e) => {
 
+      // prevent submit
       e.preventDefault();
+
+      // get the text value
       let val = userText.value;
 
+      // create li elememt
+      let task = document.createElement('li');
 
 
+      // set li value with delete btn as html template
+      task.innerHTML = `${val} <button> X </button>`;
+
+      // append li to ul
+      taskList.appendChild(task)
   })
 
 
