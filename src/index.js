8d083bfe0ad042d1421d5d6a62a17540b8e1f762
Michael Ballard <ballard.michael9@gmail.com>
2020-11-16 03:22:57 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let userText = document.querySelector('#new-task-description');
+
+  let form = document.querySelector('#create-task-form');
+
+  let taskList = document.querySelector('#tasks')
+
+  let taskContainer = document.querySelector('#list')
+
+  form.addEventListener('submit', (e) => {
+
+      // prevent submit
+      e.preventDefault();
+
+      // get the text value
+      let val = userText.value;
+
+      // create li elememt
+      let task = document.createElement('li');
+
+
+      // set li value with delete btn as html template
+      task.innerHTML = `${val}  <button class='del'> X </button>`;
+
+      // append li to ul
+      taskList.appendChild(task)
+  })
+
+  taskContainer.addEventListener('click', (e) => {
+      // e.target helps with event delegation
+      if(e.target.classList[0] === 'del'){
+          e.target.parentNode.remove()
+      }
+  })
+
+
 });
